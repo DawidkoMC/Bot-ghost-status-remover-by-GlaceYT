7ce5e976470d9f538f4661576f887dc2f998e2fa
DawidkoMC <83132245+DawidkoMC@users.noreply.github.com>
2025-01-06 01:57:02 +0100

    index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["😣 Jestem niewolnikiem Dawida", "🎮 Gram w ROBLOXA!", "📚 Czytam tickety!"];
+const statusTypes = [ 'dnd', 'idle', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
